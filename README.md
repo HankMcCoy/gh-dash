fea83358a581fce91d9976de766944e672296694
Thomas Beirne <thomas@buildingconnected.com>
2017-05-27 12:06:54 -0700

    Start trying to get basic DB population occurring

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This is a WIP tool for querying GH data, without having an Enterprise installation.
---
+++ jobs/populate-latest-data.js
@@ -0,0 +1,112 @@
+#! /usr/bin/env node
+'use strict'
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const _ = require('lodash')
+const compact = require('lodash/fp/compact')
+const flow = require('lodash/fp/flow')
+const sortBy = require('lodash/fp/sortBy')
+const first = require('lodash/fp/first')
+const request = require('superagent-promise')(require('superagent'), Promise)
+const querystring = require('querystring')
+
+// Connection URL
+const mongoConnectionStr = 'mongodb://localhost:27017/gh-dash'
+
+const org = 'buildingconnected'
+const repo = 'client'
+const token = process.env.GH_TOKEN
+const api = 'https://api.github.com'
+const repoUrl = `${api}/repos/${org}/${repo}`
+const getJson = url => request.get(url).end().then(res => res.body)
+const getGhJson = ({ path, query }) => {
+  let queryStr = querystring.stringify(query)
+  queryStr = queryStr
+    ? `${queryStr}&access_token=${token}`
+    : `access_token=${token}`
+
+  return getJson(`${repoUrl}/${path}?${queryStr}`)
+}
+
+function getProcessedPrs() {
+  return getPrNumbers().then(getActualPrsWithEvents).then(createFinalPrObjs)
+}
+
+function getPrNumbers() {
+  return getGhJson({
+    path: 'pulls',
+    query: { per_page: '100' },
+  }).then(prs => prs.map(pr => pr.number))
+}
+
+function getActualPrsWithEvents(prNumbers) {
+  return Promise.all(
+    prNumbers.map(number =>
+      Promise.all([
+        getGhJson({
+          path: `pulls/${number}`,
+        }),
+        getGhJson({
+          path: `issues/${number}/events`,
+          query: { per_page: '100' },
+        }),
+      ]).then((pr, events) => ({ pr, events }))
+    )
+  )
+}
+
+const isGtg = event => _.get(event, 'label.name') === 'good to go'
+const getLastGtgEvent = events => _.last(events.filter(isGtg))
+
+function createFinalPrObjs(prsAndEvents) {
+  return prsAndEvents.map(({ pr, events }) => {
+    const gtgEvent = getLastGtgEvent(events)
+    const commenters = _.uniq(
+      events.map(e => _.get(e, 'comment.user.login')).filter(x => x)
+    )
+    const needsRevisionEvents = events.filter(
+      e => _.get(e, 'label.name') === 'needs revision/discussion'
+    )
+    const firstReviewLabelChangeEvent = flow(
+      compact,
+      sortBy('created_at'),
+      first
+    )([gtgEvent].concat(needsRevisionEvents))
+
+    return {
+      id: pr.id,
+      number: pr.number,
+      author: pr.user.login,
+      additions: pr.additions,
+      deletions: pr.deletions,
+      dateCreated: pr.created_at,
+      dateClosed: pr.closed_at,
+      dateMerged: pr.merged_at,
+      gtgReviewer: gtgEvent.actor.login,
+      commenters,
+      numRevisions: needsRevisionEvents.length,
+      times: {
+        waitingForReview: null,
+        spentInReview: null,
+        afterReviewBeforeMerge: null,
+      },
+    }
+  })
+}
+
+// Use connect method to connect to the server
+MongoClient.connect(mongoConnectionStr).then(
+  db => {
+    console.log('Connected successfully to server')
+
+    const getProcessedPrsPromise = getProcessedPrs()
+    getProcessedPrsPromise.then(finalPrs => {
+      console.log(finalPrs)
+    })
+
+    db.close()
+  },
+  err => {
+    assert.equal(null, err)
+  }
+)
