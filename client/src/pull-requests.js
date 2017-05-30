28d791aa7faef7c20a01aeee7c98981a02856bf6
Thomas Beirne <thomas@buildingconnected.com>
2017-05-29 18:35:33 -0700

    Fix key error

--- client/src/pull-requests.js
+++ client/src/pull-requests.js
@@ -4,12 +4,12 @@ import Pr from './pr'
 import Spacer from './spacer'
 import intersperse from './util/intersperse'
 
-const addMargins = intersperse(<Spacer height="20px" />)
+const addMargins = intersperse(i => <Spacer height="20px" key={i} />)
 
 const PullRequests = ({ pullRequests }) => (
   <div>
     <h1>Pull Requests</h1>
-    {addMargins(pullRequests.map(pr => <Pr pr={pr} />))}
+    {addMargins(pullRequests.map(pr => <Pr pr={pr} key={pr._id} />))}
   </div>
 )
 
--- client/src/util/intersperse.js
+++ client/src/util/intersperse.js
@@ -3,6 +3,9 @@ import flatMapOrig from 'lodash/fp/flatMap'
 const flatMap = flatMapOrig.convert({ cap: false })
 
 const intersperse = separator =>
-  flatMap((v, i, values) => (i === values.length - 1 ? [v] : [v, separator]))
+  flatMap((v, i, values) => {
+    const s = typeof separator === 'function' ? separator(i) : separator
+    return i === values.length - 1 ? [v] : [v, s]
+  })
 
 export default intersperse
