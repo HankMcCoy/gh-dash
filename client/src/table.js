652a650e4afc11b278d08071d2453ba30606c9f3
Thomas Beirne <thomas@buildingconnected.com>
2017-07-03 12:56:15 -0700

    Make tables sortable

--- client/src/table.js
+++ client/src/table.js
@@ -1,4 +1,6 @@
+import _ from 'lodash'
 import React from 'react'
+import { withState } from 'recompose'
 import glamorous, { Div } from 'glamorous'
 
 const Wrapper = glamorous.div({
@@ -11,8 +13,16 @@ const Head = glamorous.div(rowStyle)
 const Row = glamorous.div(rowStyle)
 
 const cellStyle = { height: '30px' }
-const HeadCell = glamorous.div(cellStyle, { fontWeight: 'bold' })
+const HeadCell = glamorous.div(cellStyle, {
+  fontWeight: 'bold',
+  cursor: 'pointer',
+})
 const RowCell = glamorous.div(cellStyle)
+
+const SortDir = {
+  ASC: 1,
+  DESC: -1,
+}
 
 const getValue = ({ col, rowData }) => {
   if (col.key) {
@@ -25,17 +35,53 @@ const getValue = ({ col, rowData }) => {
   throw new Error('Must define either `key` or `getValue` for each column')
 }
 
-const Table = ({ columns, data }) => {
+const Table = ({
+  columns,
+  data,
+  sortColIdx,
+  sortDir,
+  setSortColIdx,
+  setSortDir,
+}) => {
+  const sortCol = columns[sortColIdx]
+  let sortedRows = _.sortBy(data, rowData => {
+    let value = getValue({ col: sortCol, rowData })
+    if (typeof value === 'string') {
+      value = value.toLowerCase()
+    }
+    return value
+  })
+  if (sortDir === SortDir.DESC) {
+    sortedRows = _.reverse(sortedRows)
+  }
+
   return (
     <Wrapper>
       <Head>
-        {columns.map(col => (
-          <HeadCell css={{ flex: col.flex || '1 0 0%' }} key={col.header}>
-            {col.header}
-          </HeadCell>
-        ))}
+        {columns.map((col, idx) => {
+          const sortIndicator = sortDir === SortDir.ASC
+            ? <span> ⇣</span>
+            : <span> ⇡</span>
+          return (
+            <HeadCell
+              css={{ flex: col.flex || '1 0 0%' }}
+              key={col.header}
+              onClick={() => {
+                if (idx !== sortColIdx) {
+                  setSortColIdx(idx)
+                } else {
+                  setSortDir(
+                    sortDir === SortDir.ASC ? SortDir.DESC : SortDir.ASC
+                  )
+                }
+              }}
+            >
+              {col.header}{idx === sortColIdx ? sortIndicator : ''}
+            </HeadCell>
+          )
+        })}
       </Head>
-      {data.map((rowData, rowIdx) => (
+      {sortedRows.map((rowData, rowIdx) => (
         <Row key={rowIdx}>
           {columns.map(col => {
             const value = getValue({ col, rowData })
@@ -51,4 +97,7 @@ const Table = ({ columns, data }) => {
   )
 }
 
-export default Table
+export default _.flowRight(
+  withState('sortColIdx', 'setSortColIdx', 0),
+  withState('sortDir', 'setSortDir', SortDir.ASC)
+)(Table)
